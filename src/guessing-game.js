7c6dd17bced1ca47072b6af96225c540493c87f7
Mixacb <suhoidz@tut.by>
2017-09-24 19:44:09 +0300

    First commit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {	
+	this.gues=0;
+	this.lastmax=0;
+	this.lastmin=0;
+}
 
     setRange(min, max) {
+	this.lastmax=max;
+	this.lastmin=min;
 
     }
 
     guess() {
+	if((this.lastmax-this.lastmin)%2==0)
+	{this.gues=this.lastmin+(this.lastmax-this.lastmin)/2;
+	return this.gues;}
+	else
+	{this.gues=this.lastmin+(this.lastmax-this.lastmin+1)/2;
+	return this.gues;}
 
     }
 
     lower() {
-
+	this.lastmax=this.gues;
     }
 
     greater() {
-
+	this.lastmin=this.gues;
     }
 }
 
